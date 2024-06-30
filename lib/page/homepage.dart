b3ad6a1cc046ab6aefbd82c76d0c0f39ec9a8c7b
roningrum <rizkioktavianingrum@gmail.com>
2024-06-30 15:39:59 +0700

    Learn Flutter hari 1

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_movie_clbk/theme/theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
@@ -12,17 +17,62 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          title: const Text("Hello Cuk", style: TextStyle()),
-        ),
-        body: const Column(
-          children: [
-            Text("Hello")
-          ],
-        )
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: backgroundColor,
+          body: Column(
+            children: [
+              Container(
+                width:  MediaQuery.of(context).size.width,
+                height: 364,
+                padding: const EdgeInsets.all(16.0),
+                color: primaryColor,
+                child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset("asset/images/reward.svg", width: 24, height: 24),
+                        const SizedBox(width: 12),
+                        Container(
+                          width: 276,
+                          height: 43,
+                          alignment: Alignment.center,
+                          decoration: const BoxDecoration(
+                              color: lilacColor,
+                            borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                            child: Row(
+                              children: [
+                                SvgPicture.asset("asset/images/search.svg", width: 24, height: 24),
+                                const SizedBox(width: 12),
+                                Text("Search \"Your Payments\"", style: fieldTextStyle.copyWith(color: Colors.white, fontSize: 12)),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        SvgPicture.asset("asset/images/notification.svg", width: 24, height: 24),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 24),
+                      child: const Column(
+                        children: [
 
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              )
+            ],
+          )
+      
+      ),
     );
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,6 +2,9 @@ import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const primaryColor = Color(0xff304fff);
+const lilacColor = Color(0xff778BFE);
 const backgroundColor = Color(0xffF7F7F7);
 const textColor = Color(0xff5A5A5A);
-// const headingTextStyle = TextStyle(fontFamily)
+
+const headingTextStyle = TextStyle(fontFamily:"Poppins", fontWeight: FontWeight.w700 );
+const fieldTextStyle = TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w500);
